fc5b83d2ddc42e09f9f0fde4441524d0a96c8e0a
Irisu <nekomarisakirisame@gmail.com>
2023-05-14 19:04:59 -0400

    add omit utility

---
+++ src/util/Util.ts
@@ -0,0 +1,16 @@
+export class Util {
+    static omit<T, K extends keyof T>(obj: T | undefined, keys: K[]): Omit<T, K> {
+        if (!obj) {
+            return {} as Omit<T, K>;
+        }
+
+        const ret: Record<keyof T, any> | undefined = obj;
+        for (let key of Object.keys(obj)) {
+            if (keys.includes(key as K)) {
+                delete ret?.[key as keyof T];
+            }
+        }
+
+        return ret;
+    }
+}
