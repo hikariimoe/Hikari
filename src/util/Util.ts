b6ad73aec0e327d43b519a86c9d546bf35a43e5f
Irisu <nekomarisakirisame@gmail.com>
2023-05-15 08:20:53 -0400

    deep clone the utility (and add lodash incase i find smt helse is useful for)

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -1,10 +1,12 @@
+import { deepClone } from "@sapphire/utilities";
+
 export class Util {
     static omit<T, K extends keyof T>(obj: T | undefined, keys: K[]): Omit<T, K> {
         if (!obj) {
             return {} as Omit<T, K>;
         }
 
-        const ret: Record<keyof T, any> | undefined = obj;
+        const ret: Record<keyof T, any> | undefined = deepClone(obj);
         for (let key of Object.keys(obj)) {
             if (keys.includes(key as K)) {
                 delete ret?.[key as keyof T];
